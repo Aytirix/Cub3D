37cf0dce1339ae946781e294a131596594c06955
Robrakai <123163759+Robrakai@users.noreply.github.com>
2024-08-22 18:17:41 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hle-roux <hle-roux@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/22 16:51:03 by hle-roux          #+#    #+#             */
+/*   Updated: 2024/08/22 18:02:13 by hle-roux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+int main()
+{
+
+
+	return 0;
+}
+
+
+
+// --------------	Cub3d	--------------------------
+
+// 			---	 RESSOURCES	---
+//
+//	https://medium.com/@afatir.ahmedfatir/cub3d-tutorial-af5dd31d2fcf#:~:text=some%20walls%20now.-,Part%203%3A%20Rendering%3A,-so%20now%20we
+//	https://reactive.so/post/42-a-comprehensive-guide-to-cub3d/
+//	https://medium.com/@rtailidounia/raycasting-in-cub3d-42-network-project-a-practical-tutorial-using-vectors-68eeb16b3de2
+//
+
+// -> 4 parties
+//		- Map + Window 		= mlx + parsing
+//		- Raycasting 		= Maths, calculation
+//		- Rendering 		= Image + scaling
+//		- Player Mov		= mlx
+
+
